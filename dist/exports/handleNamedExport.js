d7242f88c0ba912e0e1b53251bc9d0ee8992a93e
Caleb Eby <caleb.eby01@gmail.com>
2019-07-04 13:07:35 -0700

    Remove dist from git

--- dist/exports/handleNamedExport.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleNamedExport = function (name, valueNode, path, namedExports) {
-    path.remove();
-    namedExports.set(name.name, valueNode);
-};
