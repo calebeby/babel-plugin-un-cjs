e030142160e3323f36be681e1e2bd5cdd33da54c
Caleb Eby <caleb.eby01@gmail.com>
2019-07-04 13:08:10 -0700

    Add wait-for-expect fixture for testing babel-plugin-add-module-exports

---
+++ tests/fixtures/wait-for-expect.test.ts
@@ -0,0 +1,43 @@
+import transform from '../test-util'
+
+test('wait-for-expect', async () => {
+  const input = `
+'use strict'
+
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+})
+exports.default = void 0
+
+const defaults = {}
+
+var waitForExpect = function waitForExpect(expectation) {}
+
+var _default = Object.assign(waitForExpect, {
+  defaults: defaults,
+})
+
+exports.default = _default
+module.exports = exports.default
+module.exports.default = exports.default
+`
+  const transformed = await transform(input)
+  expect(transformed).toMatchInlineSnapshot(`
+    "'use strict';
+
+    let _default2 = {};
+    _default2.default = void 0;
+    const defaults = {};
+
+    var waitForExpect = function waitForExpect(expectation) {};
+
+    var _default = Object.assign(waitForExpect, {
+      defaults: defaults
+    });
+
+    _default2.default = _default;
+    const _default3 = _default2.default;
+    export { _default3 as default };
+    _default2.default = _default3;"
+  `)
+})
